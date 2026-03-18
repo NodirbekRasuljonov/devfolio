3ec5e95ce5bffe9f83b4d4a50ad8c0d2e9f3fc40
Nodirbek Rasuljonov <nodirbekrasuljonov@Nodirbeks-MacBook-Air.local>
2026-03-18 12:49:32 +0100

    added some home pages using bottom nav and page view

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,14 +1,29 @@
 import 'package:devfolio/const/colors_const.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:on_click/on_click.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late PageController pageController;
+  int selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(initialPage: 0);
+  }
+
+
+
+  @override
   Widget build(BuildContext context) {
-    int selectedIndex=2;
+    
     return SafeArea(
       child: Scaffold(
         backgroundColor: ColorsConst.kMainColor,
@@ -38,37 +53,96 @@ class HomePage extends StatelessWidget {
           centerTitle: false,
           backgroundColor: ColorsConst.kMainColor,
         ),
+        body: PageView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: 4,
+          controller: pageController,
+          onPageChanged: (index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          itemBuilder: (context, index) {
+            return Container(
+              color: ColorsConst.kMainColor,
+              child: Center(
+                child: Text(
+                  "Page ${index + 1}",
+                  style: TextStyle(
+                    color: ColorsConst.kWhiteColor,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+
         bottomNavigationBar: BottomNavigationBar(
           backgroundColor: ColorsConst.kMainColor,
+          showUnselectedLabels: true,
           items: [
             BottomNavigationBarItem(
-             
-              icon: SvgPicture.asset("assets/svg/home.svg"),
+              icon: SvgPicture.asset(
+                "assets/svg/home.svg",
+                color: selectedIndex == 0
+                    ? ColorsConst.kCircleColor.withOpacity(0.9)
+                    : ColorsConst.kWhiteColor,
+              ),
               label: "Home",
             ),
             BottomNavigationBarItem(
-              icon: SvgPicture.asset("assets/svg/projects.svg"),
+              icon: SvgPicture.asset(
+                "assets/svg/projects.svg",
+                color: selectedIndex == 1
+                    ? ColorsConst.kCircleColor.withOpacity(0.9)
+                    : ColorsConst.kWhiteColor,
+              ),
               label: "Projects",
             ),
             BottomNavigationBarItem(
-              icon: SvgPicture.asset("assets/svg/cv.svg"),
+              icon: SvgPicture.asset(
+                "assets/svg/cv.svg",
+                color: selectedIndex == 2
+                    ? ColorsConst.kCircleColor.withOpacity(0.9)
+                    : ColorsConst.kWhiteColor,
+              ),
               label: "Resume",
             ),
             BottomNavigationBarItem(
-              icon: SvgPicture.asset("assets/svg/contact.svg"),
+              icon: SvgPicture.asset(
+                "assets/svg/contact.svg",
+                color: selectedIndex == 3
+                    ? ColorsConst.kCircleColor.withOpacity(0.9)
+                    : ColorsConst.kWhiteColor,
+              ),
               label: "Contact",
             ),
           ],
-          selectedItemColor: ColorsConst.kCircleColor.withOpacity(0.9),
-          selectedIconTheme: IconThemeData(
-            color: ColorsConst.kCircleColor.withOpacity(0.9),
-            ),
-            currentIndex:selectedIndex ,
-            onTap: (index){
-              selectedIndex=index;
-            },
+          selectedItemColor: ColorsConst.kCircleColor.withOpacity(0.9), // label color when selected
+          unselectedItemColor: ColorsConst.kMainColor, // label color when not selected
+          currentIndex: selectedIndex,
+          onTap: (index) {
+            debugPrint("Selected Index: $index");
+            if (index != selectedIndex) {
+              setState(() {
+                selectedIndex = index;
+              });
+              pageController.animateToPage(
+                index,
+                duration: Duration(milliseconds: 100),
+                curve: Curves.easeInOut,
+              );
+            }
+          },
         ),
       ),
     );
+  }
+   @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
   }
 }
