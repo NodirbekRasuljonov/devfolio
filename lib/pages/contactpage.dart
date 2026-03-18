8870ad900e3cb6709b338c2c9b7ad247047b63e2
NodirbekRasuljonov <nodirbekmaqsudjonovich@gmail.com>
2026-03-18 15:33:38 +0100

    contact page ui improvements

--- lib/pages/contactpage.dart
+++ lib/pages/contactpage.dart
@@ -1,5 +1,6 @@
 import 'package:devfolio/const/colors_const.dart';
 import 'package:flutter/material.dart';
+import 'package:on_click/extensions/click_extension.dart';
 
 Scaffold ContactPage() {
   return Scaffold(
@@ -40,29 +41,70 @@ Scaffold ContactPage() {
         ),
       ],
     ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Card(
+    body: SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            contactCard(
+              title: "EMAIL ME",
+              subtitle: "nodirbekmaqsudjonovich@gmail.com",
+              icon: Icons.email_outlined,
+              onTab: () {
+                debugPrint("email Pressed");
+              },
+            ),
+            SizedBox(height: 20.0,),
+            contactCard(
+              title: "CALL ME",
+              subtitle: "+48 51 733 87 92",
+              icon: Icons.phone_outlined,
+              onTab: () {
+                debugPrint("Phone Pressed");
+              },
+            ),
+            Container(height: 500.0,color: Colors.amber,)
             
-            shape: RoundedRectangleBorder(
-              
-              borderRadius: BorderRadius.circular(16),
-            ),
-            color: ColorsConst.kCircleColor.withOpacity(0.1),
-            child: ListTile(
-              titleTextStyle: TextStyle(color: ColorsConst.kWhiteColor,),
-              subtitleTextStyle: TextStyle(color: ColorsConst.kWhiteColor),
-              
-              leading: CircleAvatar(child: Icon(Icons.email)),
-              title: Text('EMAIL ME'),
-              subtitle: Text('hello@techflow.dev'),
-              trailing: Icon(Icons.arrow_outward),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     ),
   );
+}
+
+Widget contactCard({
+  required String title,
+  required String subtitle,
+  required IconData icon,
+  required VoidCallback onTab,
+}) {
+  return Container(
+    height: 90.0,
+    alignment: Alignment.center,
+    decoration: BoxDecoration(
+      color: ColorsConst.kCircleColor.withValues(alpha: 0.1),
+      borderRadius: BorderRadius.circular(16.0),
+    ),
+    child: ListTile(
+      titleTextStyle: TextStyle(
+        color: ColorsConst.kCircleColor.withValues(alpha: 0.9),
+      ),
+      subtitleTextStyle: TextStyle(
+        color: ColorsConst.kWhiteColor,
+        fontWeight: FontWeight.w400,
+      ),
+      iconColor: ColorsConst.kWhiteColor,
+      leading: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        child: Icon(
+          icon,
+          color: ColorsConst.kCircleColor.withValues(alpha: 0.9),
+        ),
+      ),
+      title: Text(title),
+      subtitle: Text(subtitle),
+      trailing: Icon(Icons.arrow_outward_outlined),
+    ),
+  ).onClick(onTab);
 }
