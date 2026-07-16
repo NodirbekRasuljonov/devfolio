899422e05c579c5883ccd059d0d4f7bc7f0803c2
NodirbekRasuljonov <NodirbekRasuljonov@users.noreply.github.com>
2026-07-16 17:21:18 +0000

    deploy: 2529e118f97df26eced55b476df92009bbf31571

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3006975667" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2682993757" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
